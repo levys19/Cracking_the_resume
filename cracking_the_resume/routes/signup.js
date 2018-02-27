e3b9771d6d6fcfeee71f136572b4ed283b7e4dd8
shallyguo <shallygu@buffalo.edu>
2018-02-27 10:03:01 -0500

    edits to the signup.js route

--- cracking_the_resume/routes/signup.js
+++ cracking_the_resume/routes/signup.js
@@ -1,8 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+// var User = require('../Models/user');
+
 router.get('/', function(req, res, next) {
   res.render('signUp', { title: 'signup page' });
 });
+
+// router.post('/', function(req, res, next) {
+//     User.create(req.body.user.firstName);
+//
+//     console.log(firstName);
+//
+// });
+//
+// app.post("/blogs", function(req, res){
+//     //create blog
+//     req.body.blog.body = req.sanitize(req.body.blog.body);
+//     Blog.create(req.body.blog, function(err, newBlog){
+//         if(err){
+//             //render new for again
+//             res.render("new");
+//         }
+//         else{
+//             //then redirect to the index
+//             res.redirect("/blogs");
+//         }
+//
+//     });
+// });
 
 module.exports = router;
