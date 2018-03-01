deb1e006bdfa0380bc556f355acb3950d1b660fb
Levy Shi <levyshi@buffalo.edu>
2018-03-01 16:08:14 -0500

    made post request from the signup page

--- cracking_the_resume/routes/split.js
+++ cracking_the_resume/routes/split.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
     res.render('split', { title: 'Split page' });
+    console.log(req.body)
+    console.log(req.body.firstName)
+    console.log(req.body.lastName)
+
 });
 
 module.exports = router;
