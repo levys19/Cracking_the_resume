bce1ffef6ea496ad8d2534b7f070639098abcac0
sravanika <sravanik@buffalo.edu>
2018-03-05 14:16:04 -0500

    Added a hash password and unhash password functions

---
+++ cracking_the_resume/Models/user.js
@@ -0,0 +1,40 @@
+var mongoose = require("mongoose");
+var bcrypt = require('bcryptjs');
+
+
+var userSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    Email: String,
+    UserName: String,
+    Password: String,
+    Year: String,
+    Major: String,
+    Seeking:String,
+    Resume:[
+        //Object Reference
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Resume"
+        }
+    ],
+    created: {type: Date, default: Date.now}
+});
+
+
+//get the user by ID
+module.exports.getUserById = function(id, callback){
+    User.findById(id, callback); };
+
+//Call this function after creating the user in the database to hash the password
+module.exports.hashPassword = function(password){
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(100), null);
+};
+
+//checking if the hash password matches the candidate password--given by the user
+module.exports.checkValidPassword = function(candidatePassword) {
+    return bcrypt.compareSync(candidatePassword, this.password);
+};
+
+//exporting module
+module.exports = mongoose.model("User", userSchema);
