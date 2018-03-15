dcee6bfc070c83152fb85b4ae9634a625b0070f6
YongJi <yongjichen97@gmail.com>
2018-03-15 18:59:18 -0400

    popup window for each resume

--- cracking_the_resume/public/Javascript/viewing.js
+++ cracking_the_resume/public/Javascript/viewing.js
@@ -1,19 +1,7 @@
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('img1');
-var modalImg = document.getElementById("img1");
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
+$(document).ready(function(){
+  $(".tile__img").click(function(){
+    var clickedImg = $(this).attr('src');
+    $("#imagePop").attr('src',clickedImg);
+    $("#tallModal").modal("toggle");
+  })
+});
