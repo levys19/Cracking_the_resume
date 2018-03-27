7473427a130bdb5f490ad6575852389db9765e82
shallyguo <shallygu@buffalo.edu>
2018-03-27 02:48:18 -0400

    create comment records from accounts page

--- cracking_the_resume/routes/accounts.js
+++ cracking_the_resume/routes/accounts.js
@@ -1,36 +1,52 @@
-var express = require('express');
-var app = require('express')();
+var express = require('express');;
 var router = express.Router();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+
+var mongoose = require('mongoose'); 
+
+//Retreiving the comment schema 
+var Comment = require('../Models/comments');
+var Resume = require("../Models/resume"); 
 
 
 router.get('/', function(req, res, next) {
     res.render('account.ejs', { title: 'accounts page', user:req.user });
+    // console.log(req.user); 
+    // console.log(req.user.Resume); 
+    // console.log(req.user.Resume._id);
+    // Resume.find({_id: req.user.Resume}, function(err, resumeRecord){
+    //   if(err){
+    //     console.log("resume could not be retrieved");
+    //   }
+    //   else{
+    //     console.log("resume was retrieved"); 
+    //     console.log(resumeRecord);
+    //   }
+    // })
 });
 
+router.post('/', function(req, res, next){
+  res.render('account', { title: 'accounts page', user:req.user });
+  console.log("this is from the post request")
+  console.log(req.body.Message); 
 
-
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname + '/account.html');
-// });
-
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  //creating comment record
+  commentRecord = new Comment({
+    Resume: req.user.Resume, 
+    User: req.user._id, 
+    content: req.body.Message 
+  }); 
+  //saving the comment record onto the database 
+  commentRecord.save(function(err, comment){
+    if(err){
+      console.log("comment record could not be created"); 
+    }
+    else{
+      console.log("comment record was created"); 
+      console.log(comment); 
+    }
   });
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-  });
-});
+   
+})
 
-// http.listen(port, function(){
-//   console.log('listening on *:' + port);
-// });
 
 module.exports = router;
