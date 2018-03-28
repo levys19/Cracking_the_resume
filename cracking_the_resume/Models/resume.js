24df4add767ffe9d94d0b238bc0ac3e3f4957e8b
shallyguo <shallygu@buffalo.edu>
2018-03-28 05:03:06 -0400

    comments in accounts page

--- cracking_the_resume/Models/resume.js
+++ cracking_the_resume/Models/resume.js
@@ -1,18 +1,36 @@
 var mongoose = require("mongoose");
 
+// //RESUME schema
+// var resumeSchema = new mongoose.Schema({
+//     resumeName: String, //NEED TO FIND TYPE OF PDF FILE
+//     comments:[
+//         //Object Reference
+//         {
+//             type: mongoose.Schema.Types.ObjectId,
+//             ref: "Comments"
+//         }
+//     ],
+//     upvotes: Number, //SOME KIND OF QUERY ?
+//     downvotes: Number //SOME KIND OF QUERY???
+// });
+
 //RESUME schema
 var resumeSchema = new mongoose.Schema({
     resumeName: String, //NEED TO FIND TYPE OF PDF FILE
     comments:[
         //Object Reference
         {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Comments"
+            userName: String,
+            content: String, 
+            created: {type: Date, default: Date.now}
         }
     ],
     upvotes: Number, //SOME KIND OF QUERY ?
     downvotes: Number //SOME KIND OF QUERY???
 });
+
+
+
 
 //exporting module
 module.exports = mongoose.model("Resume", resumeSchema);
--- cracking_the_resume/routes/accounts.js
+++ cracking_the_resume/routes/accounts.js
@@ -11,10 +11,6 @@ var User = require('../Models/user');
 
 
 router.get('/', function(req, res, next) {
-  console.log("this is the current user"); 
-  console.log(req.user); 
-  console.log("this is req.user.resume"); 
-  console.log(req.user.Resume)
   var id = req.user.Resume; 
   Resume.findById(id, function(err, resumeRecord){
     if(err){
@@ -29,110 +25,32 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next){
-  //res.render('account', { title: 'accounts page', user:req.user });
-  console.log("this is from the post request")
-  console.log(req.body.Message); 
-
-  //creating comment record
-  commentRecord = new Comment({
-    Resume: req.user.Resume, 
-    User: req.user._id, 
-    content: req.body.Message 
-  }); 
-  //saving the comment record onto the database 
-  commentRecord.save(function(err, comment){
-    if(err){
-      console.log("comment record could not be created"); 
-    }
-    else{
-      console.log("comment record was created"); 
-      console.log(comment)
-      console.log(comment.Resume); 
-      //updates resume's comment array with new comments 
-      var commentID = comment.Resume; 
-      Resume.update({_id: commentID}, {$push: {comments: [comment._id]}}, function(err,  resume){
-        if(!err){
-          console.log("update function")
-          console.log(resume); 
-        }
-      });
-    }
-  });
   var id = req.user.Resume; 
-  console.log("this is req.user.resume"); 
-  console.log(req.user.Resume)
 
+  //updating the resume with the comments 
+  Resume.update({_id: id}, {$push: {comments: [{userName: req.user.UserName, content: req.body.Message }]}}, function(err,  resume){
+      if(!err){
+        console.log("update function")
+        console.log(resume); 
+      }
+    });
 
-
-  Resume.findById({id}).populate('comments').exec(function(err, resume){
-    if(!err){
-      console.log("this is the find one")
-    console.log(resume); 
-    }
-    
-  })
-
-  //retreiving the current resume on accounts page 
+  //Retrieve the current resume: 
   Resume.findById(id, function(err, resumeRecord){
     if(err){
       console.log("Resume could not be retrieved");
     }
     else{
+      console.log("this is the current resume"); 
+      console.log(resumeRecord);
+      //res.render('account.ejs', {resumeRecord: resumeRecord})
 
-      console.log("Resume was retrieved from get request"); 
-      console.log(resumeRecord); 
-      console.log("this is the comment array from later function"); 
-      console.log(resumeRecord.comments); 
+      res.send({resumeRecord:resumeRecord});
+      res.redirect('accounts.js');
 
-      var commentArray = {
-        "comments": []
-      }; 
-      var commentID = resumeRecord.comments; 
-      console.log("outside")
-      //console.log(commentID[0]);
-      console.log("push manually");
-      //commentArray.push(commentID[0]); 
-
-      var counter = 0; 
-      for(var i = 0; i < commentID.length; i++){
-        Comment.findById(commentID[i], function(err, commentRecord){
-          console.log("there was an error");
-          console.log(commentID[i]) 
-          if(!err){
-            console.log("this is the item being pushed"); 
-           // console.log(commentRecord.content); 
-            //commentArray.push(commentRecord); 
-            commentArray.comments.push(commentRecord);
-            console.log("comment array inside for loop"); 
-            console.log(commentArray);  
-          }
-        });
-
-        console.log("this is i"); 
-        console.log(i); 
-      }
-      // commentID.forEach(function(comment){
-      //   Comment.findById(comment, function(err, commentRecord){
-      //     if(!err){
-      //       commentArray.push(commentRecord); 
-      //     }
-      //   });
-      // });
-
-      console.log("this is the comment array")
-      console.log(commentArray);
-
-      res.render('account.ejs', {resumeRecord: resumeRecord}, {commentArray: commentArray})
-      // Comment.find({}, function(err, comment){
-      //   if(!err){
-      //     res.render('account.ejs', {resumeRecord: resumeRecord}) //{comment: comment});
-      //   }
-      // })
     }
   }); 
 }); 
-
-        //res.render('account.ejs', {resumeRecord: resumeRecord}); 
 
 
 module.exports = router;
