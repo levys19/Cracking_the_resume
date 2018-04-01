23cc5bdcd1b976d98f63cece07f4d7999ce25d16
shallyguo <shallygu@buffalo.edu>
2018-04-01 13:21:53 -0400

    Displaying comments in accounts page

--- cracking_the_resume/Models/resume.js
+++ cracking_the_resume/Models/resume.js
@@ -1,33 +1,33 @@
 var mongoose = require("mongoose");
 
-// //RESUME schema
-// var resumeSchema = new mongoose.Schema({
-//     resumeName: String, //NEED TO FIND TYPE OF PDF FILE
-//     comments:[
-//         //Object Reference
-//         {
-//             type: mongoose.Schema.Types.ObjectId,
-//             ref: "Comments"
-//         }
-//     ],
-//     upvotes: Number, //SOME KIND OF QUERY ?
-//     downvotes: Number //SOME KIND OF QUERY???
-// });
-
 //RESUME schema
 var resumeSchema = new mongoose.Schema({
     resumeName: String, //NEED TO FIND TYPE OF PDF FILE
     comments:[
         //Object Reference
         {
-            userName: String,
-            content: String, 
-            created: {type: Date, default: Date.now}
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
         }
     ],
     upvotes: Number, //SOME KIND OF QUERY ?
     downvotes: Number //SOME KIND OF QUERY???
 });
+
+// //RESUME schema
+// var resumeSchema = new mongoose.Schema({
+//     resumeName: String, //NEED TO FIND TYPE OF PDF FILE
+//     comments:[
+//         //Object Reference
+//         {
+//             userName: String,
+//             content: String, 
+//             created: {type: Date, default: Date.now}
+//         }
+//     ],
+//     upvotes: Number, //SOME KIND OF QUERY ?
+//     downvotes: Number //SOME KIND OF QUERY???
+// });
 
 
 
