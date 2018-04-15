e2c0b384ce07e18751b28ba40cdfa8212f78ad92
Levy Shi <levyshi@buffalo.edu>
2018-04-15 15:28:48 -0400

    env keys

--- cracking_the_resume/routes/redirect.js
+++ cracking_the_resume/routes/redirect.js
@@ -12,7 +12,7 @@ var Resume = require('../Models/resume')
 
 //my personal AWS access key, don't share it please -levy
 
-AWS.config.update({ accessKeyId: 'AKIAIQVUQQ4AAXSVZTEA', secretAccessKey: '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' });
+AWS.config.update({ accessKeyId: process.env.ACCESS_KEY_ID, secretAccessKey: process.env.SECRET_ACCESS_KEY });
 var multer = require('multer');
 var storage = multer.diskStorage({
     destination: function(req,file, cb){
