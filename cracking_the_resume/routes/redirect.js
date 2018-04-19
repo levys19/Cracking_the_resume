19b3e59f30785177b7d2d59b2e5b4169c95d5e3c
Vighnesh Iyer <viyer5@buffalo.edu>
2018-04-19 18:14:05 -0400

    Update redirect.js
    Help wanted issue solved!!!!!

--- cracking_the_resume/routes/redirect.js
+++ cracking_the_resume/routes/redirect.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var AWS = require("aws-sdk")
 var fs = require("fs")
+var nodemailer = require('nodemailer');
 
 //retrieving User and Resume schema from the database
 var User = require('../Models/user')
@@ -89,6 +90,8 @@ router.post('/', function(req, res, next) {
                 Resume: resumeRecord._id
             });
 
+ 
+
             //saving user record to the database
             userRecord.save(function(err, user){
                 if(err){
@@ -99,6 +102,109 @@ router.post('/', function(req, res, next) {
                     console.log(user);
                 }
             });
+
+// My code begins bc!!!!!
+
+                
+        'use strict';
+        const nodemailer = require('nodemailer');
+
+        // Generate test SMTP service account from ethereal.email
+        // Only needed if you don't have a real mail account for testing
+        nodemailer.createTestAccount((err, account) => {
+            // create reusable transporter object using the default SMTP transport
+            let transporter = nodemailer.createTransport({
+                host: 'smtp.gmail.com',
+                port: 587,
+                secure: false, // true for 465, false for other ports
+                auth: {
+                    user: 'crackingtheresume@gmail.com', // generated ethereal user
+                    pass: '' // generated ethereal password
+                }
+            });
+
+            // setup email data with unicode symbols
+            let mailOptions = {
+                from: 'crackingtheresume@gmail.com', // sender address
+                to: String(req.body.email), // list of receivers
+                subject: 'Hello ✔', // Subject line
+                text: 'Hello world?', // plain text body
+                html: '<b>Hello world?</b>' // html body
+            };
+
+            // send mail with defined transport object
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    return console.log(error);
+                }
+                console.log('Message sent: %s', info.messageId);
+                // Preview only available when sending through an Ethereal account
+                console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+                // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+                // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+            });
+        });    
+        
+
+           /* var app = require('express')();
+              var mailer = require('express-mailer');
+
+
+              mailer.extend(app, {
+                      from: 'crackingtheresume@gmail.com',
+                      host: 'smtp.gmail.com', // hostname 
+                      secureConnection: true, // use SSL 
+                      port: 465, // port for secure SMTP 
+                      transportMethod: 'SMTP', // default is SMTP. Accepts anything that nodemailer accepts 
+                      auth: {
+                        user: 'crackingtheresume@gmail.com',
+                        pass: '12#$TY12'
+                      }
+                });
+ 
+            app.mailer.send('email', {
+                from: 'crackingtheresume@gmail.com', // sender address
+                to: String(req.body.email), // list of receivers // REQUIRED. This can be a comma delimited string just like a normal email to field.  
+                subject: 'Test Email', // REQUIRED. 
+                otherProperty: 'Other Property' // All additional properties are also passed to the template as local variables. 
+              }, function (err) {
+                if (err) {
+                  // handle error 
+                  console.log(err);
+                  res.send('There was an error sending the email');
+                  return;
+                }
+                res.send('Email Sent');
+              });*/
+
+   
+/*
+            var transporter = nodemailer.createTransport({
+                 service: 'Gmail',
+                 auth: {
+                    user: 'crackingtheresume@gmail.com', // Your email id
+                    pass: '12#$TY12' // Your password
+                }
+            });
+
+             var mailOptions = {
+                from: 'crackingtheresume@gmail.com', // sender address
+                to: String(req.body.email), // list of receivers
+                subject: 'Welcome to crackingtheresume', // Subject line
+                text: 'Hello there! Welcome to Cracking the Resume' //, // plaintext body
+                // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
+            };
+
+            transporter.sendMail(mailOptions, function(error, info){
+                if(error){
+                    console.log(error);
+                res.json({yo: 'error'});
+                }else{
+                    console.log('Message sent: ' + info.response);
+                res.json({yo: info.response});
+                };
+            }); */
 
 
         }
