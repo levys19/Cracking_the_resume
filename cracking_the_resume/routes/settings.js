c37c1e62f1a3a414c235b506d5c1813369b0808b
sravanika <sravanik@buffalo.edu>
2018-04-23 21:24:47 -0400

    Can change passwords for the settings page and accounts page welcomes the user with their firstname

--- cracking_the_resume/routes/settings.js
+++ cracking_the_resume/routes/settings.js
@@ -11,12 +11,24 @@ router.get('/', logIn.isLoggedIn, function(req, res, next) {
 //Post Request to get the new Password from the User
 router.post('/', function(req, res, next) {
     res.render('settings.ejs', {title: 'Settings page', user: req.user});
-    //Here is the new password that the user inputted.
+    console.log("Inside the post request")
     console.log(req.body.newPassword)
 
-    //Fill in your query and update new password here
+    new_pass = User.hashPassword(req.body.newPassword);
+    console.log(new_pass)
 
+    User.update({'UserName': req.user.UserName}, {$set: {Password:new_pass}},function(err, user) {
+        if(err) {
+            console.log("Couldn't update entry")
+        }
+        else {
+            console.log("Updated Password, printing user from update")
+            console.log(user)
+        }
+
+        return user;
+
+    })
 
 });
-
 module.exports = router;
