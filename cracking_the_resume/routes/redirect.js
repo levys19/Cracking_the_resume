31b955f297ec4f8fa1f355b4264b505940fc7f4d
Levy Shi <levyshi@buffalo.edu>
2018-04-24 14:46:44 -0400

    please

--- cracking_the_resume/routes/redirect.js
+++ cracking_the_resume/routes/redirect.js
@@ -8,21 +8,13 @@ var fs = require("fs")
 //retrieving User and Resume schema from the database
 var User = require('../Models/user')
 var Resume = require('../Models/resume')
-//converting pdf to images
+//verify if a
+var mmmagic = require('mmmagic');
 
 //my personal AWS access key, don't share it please -levy
 
 AWS.config.update({ accessKeyId: process.env.ACCESS_KEY_ID, secretAccessKey: process.env.SECRET_ACCESS_KEY });
 var multer = require('multer');
-var storage = multer.diskStorage({
-    destination: function(req,file, cb){
-        cb(null, './Resumes')
-    },
-    filename: function(req, file, db){
-        db(null, file.fieldname + '-' + Date.now() + '.png')
-    }
-});
-
 
 
 //for conversion
@@ -40,7 +32,6 @@ var storage = multer.diskStorage({
 });
 
 router.post('/', function(req, res, next) {
-    res.render('redirect', { title: 'Redirect page', user:req.user });
     const fileName =  "public/" + Date.now() + '.png'
 
     var upload = multer({storage:storage}).single('resume');
@@ -58,8 +49,8 @@ router.post('/', function(req, res, next) {
 
             //creating resume record
             var resumeRecord = new Resume({
-                upvoteCount: 0, //intializing upvote count to 0 
-                downvoteCount: 0, //initializing downvote count to 0 
+                upvoteCount: 0, //intializing upvote count to 0
+                downvoteCount: 0, //initializing downvote count to 0
                 resumeName: "https://s3.amazonaws.com/crackingtheresume/" + fileName
             });
 
@@ -105,28 +96,45 @@ router.post('/', function(req, res, next) {
 
 
         var pdfImage = new PDFImage("./Resumes/temp.pdf")
-        pdfImage.convertPage(0).then(function (imagePath) {
-           // 0-th page (first page) of the slide.pdf is available as slide-0.png
-           fs.existsSync("temp-0.png") // => true
-           fs.readFile('./Resumes/temp-0.png' , function (err, data) {
-             if (err) { throw err; }
-             var s3 = new AWS.S3();
-             s3.putObject({
-               Bucket: 'crackingtheresume',
-               //change this to whatever you want to name each file please//look at above comment
-               Key: fileName,
-               Body: data,
-               ACL: 'public-read'
-             },function (resp) {
-               console.log(arguments);
-               console.log('Successfully uploaded package.');
-             });
+        var magic = new mmmagic.Magic(mmmagic.MAGIC_MIME_TYPE);
 
-           });
+        magic.detectFile("./Resumes/temp.pdf", function(err, result) {
+          if (err) throw err;
 
-        },function(err){
-           console.log(err);
+          // image/jpeg
+          if (result != "application/pdf"){
+            //sends an error if it's not a pdf
+            res.send("error bro")
+          }
+          else{
+            //renders the correct page if the image is actually a pdf
+            res.render('redirect', { title: 'Redirect page', user:req.user });
+            pdfImage.convertPage(0).then(function (imagePath) {
+               // 0-th page (first page) of the slide.pdf is available as slide-0.png
+               fs.existsSync("temp-0.png") // => true
+               fs.readFile('./Resumes/temp-0.png' , function (err, data) {
+                 if (err) { throw err; }
+                 var s3 = new AWS.S3();
+                 s3.putObject({
+                   Bucket: 'crackingtheresume',
+                   //change this to whatever you want to name each file please//look at above comment
+                   Key: fileName,
+                   Body: data,
+                   ACL: 'public-read'
+                 },function (resp) {
+                   console.log(arguments);
+                   console.log('Successfully uploaded package.');
+                 });
+
+               });
+
+            },function(err){
+               console.log(err);
+            });
+
+          }
         });
+
 
 
 
