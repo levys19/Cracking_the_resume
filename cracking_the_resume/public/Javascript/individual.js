ac4e3044515caa83d597c9d00f075b2c9e70b1d3
YongJi <yongjichen97@gmail.com>
2018-05-03 19:59:02 -0400

    show how many chars you have left when commenting

--- cracking_the_resume/public/Javascript/individual.js
+++ cracking_the_resume/public/Javascript/individual.js
@@ -16,54 +16,54 @@ if($(".qty2").attr("id") == 1){
   //disables submit if comment is empty or contains all spaces only
   $("#postIt").prop('disabled', true);
 
-  var text_max = 50;
-  $('#count_message').html(text_max + ' words remaining');
-  $('#countLeft').html(text_max + ' words left');
+  // var text_max = 50;
+  // $('#count_message').html(text_max + ' words remaining');
+  // $('#countLeft').html(text_max + ' words left');
+  //
+  // $("#Message").on('keyup', function(){
+  //   if($.trim($('#Message').val()).length > 0) {
+  //     $("#postIt").prop('disabled', false);
+  //   }
+  //   else{
+  //     $("#postIt").prop('disabled', true);
+  //   }
+  // })
 
-  $("#Message").on('keyup', function(){
-    if($.trim($('#Message').val()).length > 0) {
-      $("#postIt").prop('disabled', false);
-    }
-    else{
-      $("#postIt").prop('disabled', true);
-    }
-  })
-
-  $('#Message').keyup(function() {
-    // alert($("#Message").val().length);
-
-    var text_length = $('#Message').val().length;
-    var text_remaining = text_max - text_length;
-    // $('#count_message').html(text_remaining + ' words remaining');
-    var words = this.value.match(/\S+/g).length;
-
-    // if(!$.trim($("#Message").val())) {
-    //   alert("empty");
-    // }
-
-    // if($("#Message").val().trim() == " "){
-    //   // var trimmed = $(this).val().split(/\s+/, 50).join(" ");
-    //   // $(this).val(trimmed + " ");
-    //   // $("#Message").val() = "";
-    //   alert("uhh");
-    // }
-
-
-    // if($("#Message").val().length == 0){
-    //   $("#postIt").prop('disabled',true);
-    // }
-
-
-    if (words > 50) {
-      var trimmed = $(this).val().split(/\s+/, 50).join(" ");
-      $(this).val(trimmed + " ");
-      }
-      else {
-        var wordsLeft = 50-words;
-          $('#count_message').text("Total words: " + words);
-          $('#countLeft').text("Words left: " + wordsLeft);
-      }
-    });
+  // $('#Message').keyup(function() {
+  //   // alert($("#Message").val().length);
+  //
+  //   var text_length = $('#Message').val().length;
+  //   var text_remaining = text_max - text_length;
+  //   // $('#count_message').html(text_remaining + ' words remaining');
+  //   var words = this.value.match(/\S+/g).length;
+  //
+  //   // if(!$.trim($("#Message").val())) {
+  //   //   alert("empty");
+  //   // }
+  //
+  //   // if($("#Message").val().trim() == " "){
+  //   //   // var trimmed = $(this).val().split(/\s+/, 50).join(" ");
+  //   //   // $(this).val(trimmed + " ");
+  //   //   // $("#Message").val() = "";
+  //   //   alert("uhh");
+  //   // }
+  //
+  //
+  //   // if($("#Message").val().length == 0){
+  //   //   $("#postIt").prop('disabled',true);
+  //   // }
+  //
+  //
+  //   if (words > 50) {
+  //     var trimmed = $(this).val().split(/\s+/, 50).join(" ");
+  //     $(this).val(trimmed + " ");
+  //     }
+  //     else {
+  //       var wordsLeft = 50-words;
+  //         $('#count_message').text("Total words: " + words);
+  //         $('#countLeft').text("Words left: " + wordsLeft);
+  //     }
+  //   });
 
 
     //
@@ -72,5 +72,14 @@ if($(".qty2").attr("id") == 1){
     //     $("#submitForm").preventDefault();
     //   }
     // });
+
+    var letterLimit = 250;
+    var textarea = document.getElementById("Message");
+
+    document.getElementById("count_message").innerHTML  = letterLimit;
+
+    textarea.addEventListener('input', function() {
+      document.getElementById("count_message").innerHTML = letterLimit - textarea.value.length;
+    }, false);
 
 });
